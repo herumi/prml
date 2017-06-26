cbeb3485927ed19566a91e5d86408311aa596620
MITSUNARI Shigeo <herumi@nifty.com>
2017-06-26 20:41:11 +0900

    prml-revised

--- mk.bat
+++ mk.bat
@@ -1,3 +1,16 @@
-platex main.tex
-platex main.tex
-dvipdfmx main.dvi
+if /i "%1"=="public" (
+	set src=main-pub
+) else (
+	set src=main
+)
+for %%x in (*.jpg *.png) do extractbb %%x
+rem for %%x in (prml0 prml1 prml2 prml3 prml4 prml5 prml9 prml10 prml11) do python add_idx.py %%x.tex
+perl book-index.pl
+platex %src%.tex
+mendex -g -U %src%.idx
+platex %src%.tex
+mendex -g -U %src%.idx
+platex %src%.tex
+mendex -g -U %src%.idx
+platex %src%.tex
+dvipdfmx %src%.dvi
